c705efd1f1a727387dd86c33bdfe27fe39c72d81
Vitaly Vorobyev <vit.vorobiev@gmail.com>
2021-12-03 15:35:14 +0700

    clusters classification done

--- clulib.py
+++ clulib.py
@@ -26,19 +26,19 @@ def data_file(key, energy, path=None):
 def build_cluster(clu, size=5):
     clu = np.array(clu)
     bclu = np.zeros((size, size))
-    eout = 0.
     idx = np.argmax(clu[:, 0])
     cz, cp = map(int, clu[idx, 1:])
     evals = clu[:, 0]
     zvals = clu[:, 1].astype(int) - cz + size // 2
     pvals = clu[:, 2].astype(int) - cp + size // 2
+    eout = 0.
     for e, z, phi in zip(evals, zvals, pvals):
-        if z > 0 and z < size and phi > 0 and phi < size:
+        if z >= 0 and z < size and phi >= 0 and phi < size:
             bclu[z, phi] = e
         else:
             eout += e
-    print(f'bclu shape: {bclu.shape}, eout: {eout:.3f}, eclu: {bclu.sum():.3f}')
-    return bclu, eout / bclu.sum()
+    bclu = np.append(bclu.ravel(), [eout, cz, cp])
+    return bclu
 
 
 def parse_data(key, energy, path, clusize=5, evtmax=10**10):
@@ -56,10 +56,7 @@ def parse_data(key, energy, path, clusize=5, evtmax=10**10):
                 if eventcnt > evtmax:
                     break
                 if clu:
-                    nclu, leaks = build_cluster(clu, clusize)
-                    if leaks > 0.05:
-                        print(f'leaks: {leaks:.3f}')
-                    clusters.append(nclu)
+                    clusters.append(build_cluster(clu, clusize))
 
                 if len(clusters) > 1:
                     twoclu.append(clusters[:2])
@@ -73,36 +70,95 @@ def parse_data(key, energy, path, clusize=5, evtmax=10**10):
                     clu.append([eclu, zidx, phidx])
                 else:
                     if clu:
-                        nclu, leaks = build_cluster(clu, clusize)
-                        if leaks > 0.05:
-                            print(f'leaks: {leaks:.3f}')
-                        clusters.append(nclu)
+                        clusters.append(build_cluster(clu, clusize))
                     clu = [[eclu, zidx, phidx]]
                     cluidx = idx
     return np.array(oneclu), np.array(twoclu), eventcnt
 
 
-def draw_cluster(clu):
+def cluster_to_matrix(clu, local=False):
+    size = int(np.sqrt(clu.size - 1)) if local else int(np.sqrt(clu.size - 3))
+    z = np.empty(size**2)
+    p = np.empty(size**2)
+    if local:
+        e = clu[:-1]
+        for row in range(size):
+            z[row * size:(row +  1) * size] = row
+            p[row * size:(row +  1) * size] = np.arange(size)
+    else:
+        e = clu[:-3]
+        for row in range(size):
+            z[row * size:(row +  1) * size] = row - size // 2
+            p[row * size:(row +  1) * size] = np.arange(size) + clu[-1] - size // 2
+    return z, p, e
+
+
+def draw_cluster(clu, fullgrid=True):
+    plt.figure(figsize=(12, 8))
+    if fullgrid:
+        plt.xlim((0, 120))
+        plt.ylim((0, 115))
+    plt.minorticks_on()
+    plt.grid(which='both')
+
+    z, p, e = cluster_to_matrix(clu)
+    plt.scatter(z, p, s=e * 100)
+    plt.tight_layout()
+
+
+def draw_cluster_local(clu):
+    plt.figure(figsize=(8, 8))
+    plt.xticks(range(5))
+    plt.yticks(range(5))
+    plt.grid()
+    z, p, e = cluster_to_matrix(clu, local=True)
+    plt.scatter(z, p, s=e * 2500)
+    plt.tight_layout()
+
+
+def draw_two_clusters(clu1, clu2):
     plt.figure(figsize=(12, 8))
     plt.xlim((0, 120))
     plt.ylim((0, 115))
     plt.minorticks_on()
     plt.grid(which='both')
-    plt.scatter(clu[:, 1], clu[:, 2], s=clu[:, 0] * 100)
+
+    z1, p1, e1 = cluster_to_matrix(clu1)
+    z2, p2, e2 = cluster_to_matrix(clu2)
+    plt.scatter(z1, p1, s=e1 * 100)
+    plt.scatter(z2, p2, s=e2 * 100)
     plt.tight_layout()
 
 
-def draw_energy_spectrum(eclu, key, epcl, xlbl):
+def draw_energy_spectrum(clusters, key, epcl, xlbl):
     plt.figure(figsize=(8, 6))
     plt.minorticks_on()
     plt.grid(which='both')
-    plt.hist(eclu, bins=100, histtype='step')
+    plt.hist(clusters[:,:-2].sum(axis=1), bins=100, histtype='step', label='Cluster energy')
+    plt.hist(clusters[:,-3], bins=100, histtype='step', label='Beyond 5x5')
     plt.title(f'{key} {epcl} MeV', fontsize=16)
     plt.xlabel(f'{xlbl} (GeV)', fontsize=14)
+    plt.legend()
     plt.tight_layout()
     plt.savefig(f'plots/{key}_{epcl}_{xlbl.replace(" ", "_")}.png')
 
 
+def draw_predictions(pred, labels, type1, type2, key):
+    plt.figure(figsize=(8, 6))
+    plt.minorticks_on()
+    plt.grid(which='both')
+    plt.hist(pred[labels==1], bins=100, histtype='step', label=type1)
+    plt.hist(pred[labels==0], bins=100, histtype='step', label=type2)
+    plt.legend()
+    plt.xlabel(f'Classifier score', fontsize=14)
+    plt.tight_layout()
+    plt.savefig(f'plots/prediction_{key}.png')
+
+
 if __name__ == '__main__':
-    cone, ctwo, neve = parse_data('pi0', 2000, './Data', clusize=5, evtmax=2)
-    # print(cone[0])
+    cone, ctwo, neve = parse_data('pi0', 2000, data_path(), clusize=5)
+    print(cone.shape)
+    # draw_cluster(cone[0])
+    draw_cluster_local(cone[0][:-2])
+    # draw_energy_spectrum(cone, 'pi0', 2000, 'Energy')
+    plt.show()
